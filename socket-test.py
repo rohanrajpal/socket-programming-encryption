65457e983400ac5bcda75d0e05bc614231c936aa
Rohan Rajpal <rohan17089@iiitd.ac.in>
2019-10-18 00:42:15 +0530

    add server code

--- socket-test.py
+++ socket-test.py
@@ -5,20 +5,26 @@ import socket
 
 def main():
     s = socket.socket()
+
     port = 12345
 
-    s.bind(('',port))
-    print("Socker binded to {}".format(port))
 
+
+
+
+def server():
+    s = socket.socket()
+    port = 12345
+    s.bind(('', port))
+    print("Socker binded to {}".format(port))
     # 5 here means that 5 connections are kept waiting if the server is busy
     s.listen(5)
-
     while True:
-
         c, addr = s.accept()
+
         print("Got connection from {}".format(addr))
 
-        c.send('Thanks man')
+        c.send(bytes('Thanks man\n', encoding='utf8'))
 
         c.close()
 
