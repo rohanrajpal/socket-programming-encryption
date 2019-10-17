7edbd7d9712b0c37d57070a16ec23af8cbe6b8fb
rohanrajpal <rohan17089@iiitd.ac.in>
2019-10-18 00:22:29 +0530

    Add server code

---
+++ socket-test.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+
+import socket
+
+
+def main():
+    s = socket.socket()
+    port = 12345
+
+    s.bind(('',port))
+    print("Socker binded to {}".format(port))
+
+    # 5 here means that 5 connections are kept waiting if the server is busy
+    s.listen(5)
+
+    while True:
+
+        c, addr = s.accept()
+        print("Got connection from {}".format(addr))
+
+        c.send('Thanks man')
+
+        c.close()
+
+
+def connect_to_google():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host_ip = socket.gethostbyname('www.google.com')
+    port = 80
+    s.connect((host_ip, port))
+    print(s)
+
+
+if __name__ == '__main__':
+    main()
