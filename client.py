b6c368cf042ef9eb9d97dee022dec145af9aa115
Rohan Rajpal <rohan17089@iiitd.ac.in>
2019-10-23 20:58:22 +0530

    Code cleanup

--- client.py
+++ client.py
@@ -13,23 +13,25 @@ def main():
 
 def client():
     s = socket.socket()
-    port = 12345
+    port = 1234
     s.connect(('', port))
 
     send_file(s)
 
     s.close()
 
+
 def get_fsize(fname):
     statinfo = os.stat(fname)
     sz = statinfo.st_size
     return sz
 
+
 def file_size(fname):
     sz = str(get_fsize(fname))
     l = len(sz)
-    sz +=(' '*(buffer_size-l))
-    print("sz",sz)
+    sz += (' ' * (buffer_size - l))
+    print("sz", sz)
     sz = bytes(sz, encoding='utf-8')
     return sz
 
@@ -41,64 +43,59 @@ def pad_data(data):
         data += bytes(' ' * (16 - remainder), encoding='utf-8')
     return data
 
+
 def num_pkt(size):
-    return int(ceil(size/buffer_size))
+    return int(ceil(size / buffer_size))
+
 
 def num_pkts_file(fname):
     return num_pkt(get_fsize(fname))
 
-def send_file(s):
 
+def send_file(s):
     fname = "mytext.txt"
     f = open(fname, mode='rb')
 
     fsize = file_size(fname)
     print("In client, file size is: " + str(fsize))
-    #sending file size
     s.send(fsize)
 
     l = f.read(buffer_size)
 
-    cnt=0
-    
+    cnt = 0
+
     while l:
-        # if cnt > 2:
-        #     break
         cnt += 1
         s.send(l)
-        print('Sent data', repr(l))
+        # print('Sent data', repr(l))
         l = f.read(buffer_size)
-        # s.send(l)
-    print("done")
-    cnt_pkt = num_pkts_file(fname)
-    print(cnt,cnt_pkt)
-    print("^Num packets")
-    # while True:
-    for i in range(cnt_pkt):
-        # len_exp = 16 + buffer_size + 16 - (buffer_size % 16)
-        len_exp = 16 + buffer_size 
-        if buffer_size%16 != 0:
-            len_exp += 16 - (buffer_size%16)
 
-        print("len_exp",len_exp)
+    # print("done")
+
+    cnt_pkt = num_pkts_file(fname)
+    # print(cnt, cnt_pkt)
+    # print("^Num packets")
+
+    for i in range(cnt_pkt):
+        len_exp = 16 + buffer_size
+        if buffer_size % 16 != 0:
+            len_exp += 16 - (buffer_size % 16)
+
+        # print("len_exp", len_exp)
         recd_data = s.recv(len_exp)
-        
-        print("lcrd",len(recd_data))
+
+        # print("lcrd", len(recd_data))
 
         iv = recd_data[:16]
-        print("len",len(iv))
+        # print("len", len(iv))
 
-        #Check
         aes = AES.new(key, AES.MODE_CBC, iv)
 
         decd = aes.decrypt(pad_data(recd_data[16:]))
-        # print(recd_data)
-        # print(recd_data[16:])
-        # print("decd",decd)
-        print(decd[:buffer_size].decode("utf-8","ignore"),end='')
-        # print(decd[:buffer_size].decode(),end='')
-        print("\n^Fin")
 
+        print(decd[:buffer_size].decode("utf-8", "ignore"), end='')
+
+        # print("\n^Fin")
 
     f.close()
 
--- server.py
+++ server.py
@@ -11,18 +11,21 @@ key = "00112233445566778899aabbccddeeff"
 
 def get_num(b):
     s = b.decode("utf-8")
-    print("fsize",s)
+    print("fsize", s)
     return int(s)
+
 
 def main():
     server()
 
+
 def num_pkt(size):
-    return int(ceil(size/buffer_size))
+    return int(ceil(size / buffer_size))
+
 
 def server():
     s = socket.socket()
-    port = 12345
+    port = 1234
     s.bind(('', port))
     print("Socker binded to {}".format(port))
     # 5 here means that 5 connections are kept waiting if the server is busy
@@ -33,56 +36,45 @@ def server():
         print("Got connection from {}".format(addr))
 
         # Encryption
+        f = open("tmp.txt", 'wb')
 
-        f =open("tmp.txt",'wb')
-
-        #receiving file size
+        # receiving file size
         fsize = c.recv(buffer_size)
-        print("len_data",len(fsize))
+        # print("len_data", len(fsize))
         fsize = get_num(fsize)
-        #expected no. of packets 
+        # expected no. of packets
         num_exp = num_pkt(fsize)
-
-
-        # while True:
-
 
         for i in range(num_exp):
             data = c.recv(buffer_size)
             data = pad_upto_buf(data)
-            print("len_data_recvd",len(data))
-            
-            # if not data:
-            #     break
+            # print("len_data_recvd", len(data))
+
             print("Received", data)
-            iv = os.urandom(16)            
-            aes = AES.new(key, AES.MODE_CBC, iv)                    
+            iv = os.urandom(16)
+            aes = AES.new(key, AES.MODE_CBC, iv)
             data = pad_data(data)
             encd = aes.encrypt(data)
-            # print("Encrypting",encd)
 
             to_send = iv + encd
-            print("len_send",len(to_send))
-            print("Writing ecrypted data", to_send)
+            # print("len_send", len(to_send))
+            # print("Writing ecrypted data", to_send)
 
             f.write(to_send)
-        print("d")   
+        # print("d")
         f = open("tmp.txt", mode='rb')
-        
 
         val_to_send = buffer_size + 16
-        if buffer_size%16 != 0:
-            val_to_send += 16 - (buffer_size%16)
+        if buffer_size % 16 != 0:
+            val_to_send += 16 - (buffer_size % 16)
 
-        print(str(val_to_send) + " " + "val to send")   
+        # print(str(val_to_send) + " " + "val to send")
 
-        # l = f.read(buf_size+16)
         l = f.read(val_to_send)
 
         while l:
-            print(str(len(l)) + " Actually sending")
+            # print(str(len(l)) + " Actually sending")
             c.send(l)
-            # l = f.read(buf_size+16)
             l = f.read(val_to_send)
 
         print('Done sending')
@@ -96,9 +88,10 @@ def pad_data(data):
         data += bytes(' ' * (16 - remainder), encoding='utf-8')
     return data
 
+
 def pad_upto_buf(data):
     data += bytes(' ' * (buffer_size - len(data)), encoding='utf-8')
-    return data    
+    return data
 
 
 if __name__ == '__main__':
